64fbc92dbbf4c6a0d2ad16e1930a91d721ef8605
Herbert Nordson <herbert_nordson@hotmail.com>
2022-05-31 10:32:14 -0300

    feat: create Netflix Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TmdbAPI from "./TmdbAPI";
+
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
+
+export default () => {
+
+	const [movieList, setMovieList] = useState([]);
+	const [featuredData, setFeaturedData] = useState(null);
+	const [blackHeader, setBlackHeader] = useState();
+
+	useEffect(() => {
+		const loadAll = async () => {
+			// Pegando os elementos da lista completa
+			let list = await TmdbAPI.getHomeList();
+			setMovieList(list);
+
+			// Pegando o Featured
+			let originals = list.filter(i=>i.slug === 'originals');
+			let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+			let chosen = originals[0].items.results[randomChosen];
+			let chosenInfo = await TmdbAPI.getMovieInfo(chosen.id, 'tv');
+			setFeaturedData(chosenInfo);
+		}
+		
+		loadAll();
+	}, []);
+
+	useEffect (()=> {
+		const scrollListener = () => {
+			if (window.scrollY > 10 ) {
+				setBlackHeader(true);
+			} else {
+				setBlackHeader(false);
+			}
+		}
+
+		window.addEventListener('scroll', scrollListener);
+		return () => {
+			window.removeEventListener('scroll', scrollListener);
+		}
+	}, []);
+
+	return (
+		<div className="page">
+			
+			<Header black={blackHeader} />
+
+			{featuredData && 
+				<FeaturedMovie  item={featuredData}/>
+			}
+
+			<section className="lists">
+				{movieList.map((item, key) => (
+					<MovieRow key={key} title={item.title} items={item.items} />
+				))}
+			</section>
+			<footer>
+				<p>Desenvolvimento por Herbert Nordson</p>
+				<p>Direitos reservados a Netflix</p>
+				<p>Conteúdo cedido por TMDB</p>
+			</footer> 
+			{movieList.length <= 0 && 
+				<div className="loading">
+					<img src="https://www.corvetteforum.com/forums/attachments/off-topic/48024333d1475661585-will-disney-buy-netflix-4f155204-7266-486d-88a5-2018ff11f947.gif" />
+				</div>
+			}
+		</div>
+	);
 }
-
-export default App;
